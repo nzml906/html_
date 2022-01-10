81461c93a32d9af01ca2819c205deabe8327a040
Nazmul <nzml444@gmail.com>
2022-01-10 19:43:11 +0600

    Sticky Navbar (jquery) on scroll

--- build/js/AdminLTE.js
+++ build/js/AdminLTE.js
@@ -3,4 +3,13 @@
 
   // Initiate the wowjs
   new WOW().init();
+
+  // Sticky Navbar
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
 })(jQuery);
