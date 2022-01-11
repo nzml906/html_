0c355394d7e9aaa62e47a8b71f65a434ebb358e5
Nazmul <nzml444@gmail.com>
2022-01-11 14:06:26 +0600

    .

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -18,6 +18,14 @@
       } else {
         $(".navbar").removeClass("sticky-top shadow-sm");
       }
+    }); // Skills
+
+    $(".skill").waypoint(function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    }, {
+      offset: "80%"
     });
   })(jQuery);
 
