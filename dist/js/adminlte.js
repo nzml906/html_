f5f14930a4b306b32a3168c6f30996fb1d5e5fff
Nazmul <nzml444@gmail.com>
2022-01-11 17:56:31 +0600

    .

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -61,6 +61,20 @@
           items: 2
         }
       }
+    }); // Back to top button
+
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 100) {
+        $(".back-to-top").fadeIn("slow");
+      } else {
+        $(".back-to-top").fadeOut("slow");
+      }
+    });
+    $(".back-to-top").on("click", function () {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 1500, "easeInOutExpo");
+      return false;
     });
   })(jQuery);
 
