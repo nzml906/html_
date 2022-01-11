13bdc753174a2ea6426fabac4769a3001b6b0896
Nazmul <nzml444@gmail.com>
2022-01-11 15:19:35 +0600

    Projects tab headers implemented with isotope lib

--- build/js/AdminLTE.js
+++ build/js/AdminLTE.js
@@ -28,4 +28,16 @@
     delay: 10,
     time: 2000,
   });
+
+  // Projects Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
 })(jQuery);
