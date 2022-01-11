24682c61e1c7be1667b665696d96c2c8b220534f
Nazmul <nzml444@gmail.com>
2022-01-11 17:56:22 +0600

    Back to top button on scroll

--- build/js/AdminLTE.js
+++ build/js/AdminLTE.js
@@ -62,4 +62,17 @@
       },
     },
   });
+
+  // Back to top button
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 })(jQuery);
