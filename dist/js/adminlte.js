d6e5a792cd3737356f8dc824f508f2bbb05d8d60
Nazmul <nzml444@gmail.com>
2022-01-11 14:24:03 +0600

    .

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -26,6 +26,11 @@
       });
     }, {
       offset: "80%"
+    }); // Facts counter
+
+    $('[data-toggle="counter-up"]').counterUp({
+      delay: 10,
+      time: 2000
     });
   })(jQuery);
 
